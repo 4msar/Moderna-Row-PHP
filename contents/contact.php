a1a8e75eac7e6cd8636e8145030c6328158f2518
Saiful Alam Rakib <17962782+4msar@users.noreply.github.com>
2018-03-28 02:08:10 +0600

    Update Contact Page
    Form Validation added

--- contents/contact.php
+++ contents/contact.php
@@ -1,6 +1,6 @@
 <?php 
 
-$success = '';
+$success = $msg = '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(isset($_POST['contact'])) {
@@ -22,11 +22,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $admin_mail = get_data('settings', 'data_key','ws_email', 'data_value');
         $to = ($admin_mail!=null ? $admin_mail : '120msar@gmail.com');
         
-        // send email
-        if (mail($to,$sub,$message, $headers)) {
-        	$success = 'true';
+        if($name!=null && $email!=null && $msg!=null){
+            // send email
+            if (mail($to,$sub,$message, $headers)) {
+            	$success = 'true';
+            }else{
+            	$msg = 'true';
+            }
         }else{
-        	echo "Try Again";
+            $msg = 'true';
         }
     }
     
@@ -47,27 +51,24 @@ $lng = get_data('settings', 'data_key','ws_c_lng', 'data_value');
 				<div class="row">
 					<div class="col-md-8 col-md-offset-2">
 						<h4>Get in touch with us by filling <strong>contact form below</strong></h4>
-						<?php if ($success!=null): ?>
-							
-							<div id="sendmessages">Your message has been sent. Thank you!</div>
+						<?php if ($success=='true'): ?>
+							<div style="display: block" id="sendmessage">Your message has been sent. Thank you!</div>
 						<?php endif ?>
-						<div id="errormessage"></div>
+						<?php if ($msg=='true'): ?>
+						    <div style="<?php echo $msg=='true' ? "display: block" : 'display: none'; ?>" id="errormessage">Please Enter Your Name, Email & Message.</div>
+						<?php endif; ?>
 						<form action="" method="post" role="form" class="contactForm">
 							<div class="form-group">
 								<input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-								<div class="validation"></div>
 							</div>
 							<div class="form-group">
 								<input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-								<div class="validation"></div>
 							</div>
 							<div class="form-group">
 								<input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-								<div class="validation"></div>
 							</div>
 							<div class="form-group">
 								<textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-								<div class="validation"></div>
 							</div>
 
 							<div class="text-center"><button name="contact" type="submit" class="btn btn-theme">Send Message</button></div>
